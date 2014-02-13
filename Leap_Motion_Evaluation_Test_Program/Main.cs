c9d0ec52c90f2f16be572b5a496454d1d00b9907
Chris Buckingham <SUPERgluedCHAIN@gmail.com>
2014-02-13 11:49:23 +0000

    Browser added

--- Leap_Motion_Evaluation_Test_Program/Main.cs
+++ Leap_Motion_Evaluation_Test_Program/Main.cs
@@ -13,6 +13,7 @@ namespace Leap_Motion_Evaluation_Test_Program
     public partial class Main : Form
     {
         int counter = 1;
+
         public Main()
         {
             InitializeComponent();
@@ -36,13 +37,49 @@ namespace Leap_Motion_Evaluation_Test_Program
             Counter_Value.Text = counter++.ToString();
 
             if (counter == 6)
-                Coutner_Button.Visible = false;
+            {
+                Coutner_Button.Enabled = false;
+                Time_Taken_Timer.Stop();
+                MessageBox.Show(Time_Taken_Timer.ToString());
+            }
+                
         }
 
         private void Show_Form2_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();
             form2.Show();
+            if (Test_Radio_Button.Checked == true)
+            {            
+                Show_Form2.Enabled = false;
+            }
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Test_Radio_Button_CheckedChanged(object sender, EventArgs e)
+        {
+            Exit.Enabled = false;
+            Time_Taken_Timer.Start();
+        }
+
+        private void Practice_Radio_Button_CheckedChanged(object sender, EventArgs e)
+        {
+            counter = 1;
+            Counter_Value.Text = 0.ToString();
+
+            Coutner_Button.Enabled = true;
+            Show_Form2.Enabled = true;
+            Exit.Enabled = true;
         }
 
     }
