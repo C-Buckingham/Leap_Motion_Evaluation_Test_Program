78a5782b70c9948540a849b265a76536b4ef6333
Chris Buckingham <chris7789@hotmail.co.uk>
2014-03-09 11:56:34 +0000

    button fix

--- Leap_Motion_Evaluation_Test_Program/Main.Designer.cs
+++ Leap_Motion_Evaluation_Test_Program/Main.Designer.cs
@@ -128,7 +128,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(372, 233);
+            this.button1.Location = new System.Drawing.Point(374, 214);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
